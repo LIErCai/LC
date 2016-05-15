d32c5983eb321bcfff8486bf715589333ee40673
Liercai <1179542975@qq.com>
2016-05-15 22:40:58 +0800

    试试

---
+++ LCBudejie/LCBudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCBudejie
+//
+//  Created by admin on 16/5/15.
+//  Copyright © 2016年 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCBudejie/LCBudejie/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LCBudejie
+//
+//  Created by admin on 16/5/15.
+//  Copyright © 2016年 LC. All rights reserved.
+//是这个吗？
+// 是 LC 我操作一遍吧  找来找去还更慢
+//  很快的 几分钟
+
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+//可以了啊 你没有添加绑定账号吧
+//你那个账号是哪一个
+//ok
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:[[UIView alloc] init]];
+
+}
+
+@end
